c2d4ff3069990d12fbfff8aa37376a28c55cae7c
Josias Denone <josias.denone@hotmail.com>
2020-04-22 00:13:23 -0300

    Frontend do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <h1>Hello World.</h1>
+    <Routes />
   );
 }
 
